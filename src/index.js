982ab21ecaa6904929cf7a902cea1a8c37008a40
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-04-28 14:15:42 -0500

    feat: bootstrap card layout added

--- src/index.js
+++ src/index.js
@@ -16,7 +16,14 @@ getMovies().then((movies) => {
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
 
-        $('#test-paragraph').append(`id#${id} - ${title} - rating: ${rating}`);
+        let renderHTML = `<div class="card">
+          <div class="card-body"> 
+          <h5 class="card-title">${id} - ${title}</h5>
+     <p class="card-text">Rating: ${rating}</p>
+      </div></div>`
+
+        // $('#test-paragraph').append(`id#${id} - ${title} - rating: ${rating}`);
+        $('#test-paragraph').append(renderHTML);
 
     });
 }).catch((error) => {
@@ -28,7 +35,6 @@ $(document).ready(() => {
 
         $('#spinner').html(`<div class="spinner-border" role="status">
   <span class="sr-only">Loading...</span></div>`);
-
 
     setTimeout(function(){
        $('#spinner').hide()
