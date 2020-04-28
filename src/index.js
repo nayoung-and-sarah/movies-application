baf9570fd2e2f135c5f913ef78ff4c00b3ce2ad5
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-04-28 11:37:02 -0500

    able to print movies to html

--- src/index.js
+++ src/index.js
@@ -7,14 +7,30 @@ sayHello('World');
 /**
  * require style imports
  */
+const $ = require('jquery');
+
+const bootStrap = require('bootstrap');
+
 const {getMovies} = require('./api.js');
 
+
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  console.log ('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $('#test-paragraph').html(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
+});
+
+$(document).ready( () => {
+  // $('body').html("Should be on page.");
+  $('#test-paragraph').html(`<div class="spinner-border" role="status">
+  <span class="sr-only">Loading...</span>
+</div>`);
+
+getMovies();
+
+
 });
