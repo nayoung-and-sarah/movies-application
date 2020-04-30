107b3772bdd0f4558081fb6893c09699569f8629
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-04-29 23:00:45 -0500

    Feat: search term resets after new movie is added

--- src/index.js
+++ src/index.js
@@ -21,27 +21,24 @@ function updateMovies() {
             let renderHTML =
                 `<div class="card">
                      <div class="card-body">
-                         <h5 id="movie-title" class="card-title">${id}- ${title}</h5>
+                         <h5 id="movie-title" class="card-title"> ${title}</h5>
                          <p id="movie-rating" class="card-text user-edit-rating">Rating: ${rating}</p>
                          <div>
-                          <button data-id="${id}" class="btn btn-sm btn-outline-dark edit-btn mr-1">Edit Movie</button>
-                          <button data-id="${id}" class="btn btn-sm btn-outline-danger delete-btn">Delete Movie</button>
-                          </div>
+                            <button data-id="${id}" class="btn btn-sm btn-outline-dark edit-btn mr-1">Edit Movie</button>
+                             <button data-id="${id}" class="btn btn-sm btn-outline-danger delete-btn">Delete Movie</button>
+                         </div>
                     </div>
                </div>`;
-
             $('.card-deck').append(renderHTML);
-            // $('#test-paragraph').append(`id#${id} - ${title} - rating: ${rating}`);
         });
+
         $('.edit-btn').click(userEditMovie);
         $('.delete-btn').click(deleteFunction)
-
     }).catch((error) => {
         alert('Oh no! Something went wrong. Check the console for details.');
         console.log(error);
     })
 }
-
 
 // allow users to edit movies
 function userEditMovie(e) {
@@ -50,24 +47,23 @@ function userEditMovie(e) {
 //     console.log($(e.target).parent());
 
     let cardBody= $(e.target).parent();
-
     let specificID = e.target.dataset.id;
 
         let renderForm = `
-    <div class="form-group mt-3">
-       <label for="exampleFormControlInput1">Movie Title</label>
-         <input type="text" class="form-control edit-title " id="exampleFormControlInput1">
-    </div>
-       <div class="form-group">
-         <label for="exampleFormControlSelect1"> Rating</label>
-         <select class="form-control edit-rating" id="exampleFormControlSelect1">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-        </select>
-       </div>
+        <div class="form-group mt-3">
+             <label for="exampleFormControlInput1">Movie Title</label>
+             <input type="text" class="form-control edit-title " id="exampleFormControlInput1">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1"> Rating</label>
+            <select class="form-control edit-rating" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+             </select>
+        </div>
         <div class="edit-btn-submit">
             <button class="btn btn-outline-dark submit-edit-btn" data-id="${specificID}">Submit edits</button>
         </div>`;
@@ -80,9 +76,8 @@ function userEditMovie(e) {
                 $(this).attr('data-id', specificID);
             let editRating = $('.edit-rating').val();
             let editTitle =$('.edit-title').val();
-
             console.log(editTitle,editRating, specificID);
-            console.log(this);
+
             editMovie(editTitle,editRating,specificID)
                 .then (updateMovies) // run after ajax
         });
@@ -108,8 +103,6 @@ $(document).ready(() => {
     $('#spinner').html(`<div class="spinner-border" role="status">
   <span class="sr-only">Loading...</span></div>`);
 
-
-
     updateMovies(); // render the HTML before button is clicked
 
     //click to post movie
@@ -126,7 +119,8 @@ $(document).ready(() => {
         //     "title": movieTitle, //adding user input from form
         //     "rating": movieRating // adding user rating from form
         // });
-        $('#exampleFormControlInput1').empty();
+
+        $('.search-term').val('');
         updateMovies();
     });
 
